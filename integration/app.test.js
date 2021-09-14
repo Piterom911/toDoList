423691245c1f88ea51947697349b65a638980c39
Roman Dogoda <piterom911@gmail.com>
2021-09-14 20:49:03 +0300

    12. Snapshots All Components

--- integration/app.test.js
+++ integration/app.test.js
@@ -1,7 +1,55 @@
 describe('addItemForm', () => {
-    it('base example, visually looks correct', async () => {
+    it('Add Item Form example, visually looks correct', async () => {
         // APIs from jest-puppeteer
         await page.goto('http://localhost:9009/iframe.html?id=app-components-add-item-form--add-new-item-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+    it('App Main example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=app-components-app-main--app&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+    it('Editable Span example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=app-components-editable-span--editable-span-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+    it('Editable Span Is Done example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=app-components-editable-span--editable-span-is-done-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+    it('Task example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=app-components-task--task-is-not-done&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+    it('Task Done example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=app-components-task--task-is-done&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+    it('To Do List example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=app-components-to-do-list--to-do-list-all&viewMode=story');
         const image = await page.screenshot();
 
         // API from jest-image-snapshot
